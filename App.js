02ae5634bfc21cd23f3870bfaa11ce4eafab2d8e
Mehul2122 <mehul.21022@gmail.com>
2020-12-13 14:59:47 +0530

    Add TextInput

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Welcome To CICD App </Text>
+      <TextInput 
+        style={{borderWidth:1,paddingHorizontal:20,marginVertical:8}}
+        placeholder="Enter Your Name"
+      />
       <StatusBar style="auto" />
     </View>
   );
